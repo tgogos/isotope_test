a0044ac93dc4f98f324017ce72dda1b8afd0e961
Anastasios Gogos <tgogos@gmail.com>
2017-06-10 01:22:39 +0300

    it works!

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # isotope_test
+
+This is quick test to see how isotope behaves with many boxes and tags.
+
+ - ~2600 random words
+ - 500 generated boxes with words
+ - words are also used as tags for every box
+
+
+ ## Configure
+
+ you can modify the following, inside `custom.js` file:
+
+ ```js
+var number_of_boxes = 500;
+var min_box_words = 25;
+var max_box_words = 60;
+ ```
+
+## Generated box example
+
+ ```html
+<div class="grid-item trust mix tense marvelous even outstanding relieved ajar plane gaze gorgeous fairies dad aware bruise group basketball permissible punishment third tick used yard muscle mundane wakeful look joyous picayune side ">
+   <div class="box">
+       trust mix tense marvelous even outstanding relieved ajar plane gaze gorgeous fairies dad aware bruise group basketball permissible punishment third tick used yard muscle mundane wakeful look joyous picayune side 
+   </div>
+</div>
+ ```
--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,34 @@
 var iso = null;
+var number_of_boxes = 500;
+var min_box_words = 25;
+var max_box_words = 60;
 
 var template =
-`<div class="handbars-tpl grid">
-   <div class="grid-item">
-       <div class="song-box">
-           
-       </div>
+`<div class="grid-item {{tags}}">
+   <div class="box">
+       {{words}}
    </div>
 </div>`;
 
-console.log(template);
+var tag_tpl = '<li class="tag">{{tag}}</li>'
+
+
 
 
 $(document).ready(function(){
+
+
+    tag_wrapper = $('.tag-wrapper');
+    for (i=0; i<words.length ; i++) {
+        tag_wrapper.append( tag_tpl.replace("{{tag}}",words[i]+" ") );
+    } 
+
+
+    grid = $('.grid');
+    for (i=0; i<number_of_boxes ; i++) {
+        box = create_box();
+        grid.append(box);
+    }
 
 
     iso = $('.grid').isotope({
@@ -42,3 +58,26 @@ $(document).ready(function(){
     //     }
     // });
 });
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function create_box() {
+    random_words = get_random_words();
+    tpl = template.replace("{{tags}}", random_words);
+    tpl = tpl.replace("{{words}}", random_words);
+    return tpl;
+}
+
+function get_random_words() {
+    num_of_words = getRandomInt(min_box_words, max_box_words);
+    word = "";
+    for (j=0 ; j<num_of_words ; j++) {
+        word += words[getRandomInt(0, words.length)] + " ";
+    }
+    // console.log(word);
+    return word;
+}
